71770d9312abf7ec19fe416c0fe14e746e8daee5
kristoffer johansson <johanson.kristoffer@gmail.com>
2020-12-18 16:53:54 +0100

    feat: export KongAPIError and expose HTTP status code

--- kong/error.go
+++ kong/error.go
@@ -4,20 +4,24 @@ import (
 	"fmt"
 )
 
-type kongAPIError struct {
+type KongAPIError struct {
 	httpCode int
 	message  string
 }
 
-func (e *kongAPIError) Error() string {
+func (e *KongAPIError) Error() string {
 	return fmt.Sprintf("HTTP status %d (message: %q)", e.httpCode, e.message)
+}
+
+func (e *KongAPIError) Code() int {
+	return e.httpCode
 }
 
 // IsNotFoundErr returns true if the error or it's cause is
 // a 404 response from Kong.
 func IsNotFoundErr(e error) bool {
 	switch e := e.(type) {
-	case *kongAPIError:
+	case *KongAPIError:
 		return e.httpCode == 404
 	default:
 		return false
