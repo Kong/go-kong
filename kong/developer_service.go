bcc7dc02629eb4cce980d8ba8a0c4eb1d7ec39f5
Christian <57408681+ChristianJacquot@users.noreply.github.com>
2021-03-24 10:47:08 +0100

    feat: add developer support

---
+++ kong/developer_service.go
@@ -0,0 +1,197 @@
+package kong
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+// AbstractDeveloperService handles Developers in Kong.
+type AbstractDeveloperService interface {
+	// Create creates a Developer in Kong.
+	Create(ctx context.Context, developer *Developer) (*Developer, error)
+	// Get fetches a Developer in Kong.
+	Get(ctx context.Context, emailOrID *string) (*Developer, error)
+	// GetByCustomID fetches a Developer in Kong.
+	GetByCustomID(ctx context.Context, customID *string) (*Developer, error)
+	// Update updates a Developer in Kong
+	Update(ctx context.Context, developer *Developer) (*Developer, error)
+	// Delete deletes a Developer in Kong
+	Delete(ctx context.Context, emailOrID *string) error
+	// List fetches a list of Developers in Kong.
+	List(ctx context.Context, opt *ListOpt) ([]*Developer, *ListOpt, error)
+	// ListAll fetches all Developers in Kong.
+	ListAll(ctx context.Context) ([]*Developer, error)
+}
+
+// DeveloperService handles Developers in Kong.
+type DeveloperService service
+
+// Create creates a Developer in Kong.
+// If an ID is specified, it will be used to
+// create a developer in Kong, otherwise an ID
+// is auto-generated.
+func (s *DeveloperService) Create(ctx context.Context,
+	developer *Developer) (*Developer, error) {
+
+	queryPath := "/developers"
+	method := "POST"
+	if developer.ID != nil {
+		queryPath = queryPath + "/" + *developer.ID
+		method = "PUT"
+	}
+	req, err := s.client.NewRequest(method, queryPath, nil, developer)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var createdDeveloper Developer
+	_, err = s.client.Do(ctx, req, &createdDeveloper)
+	if err != nil {
+		return nil, err
+	}
+	return &createdDeveloper, nil
+}
+
+// Get fetches a Developer in Kong.
+func (s *DeveloperService) Get(ctx context.Context,
+	emailOrID *string) (*Developer, error) {
+
+	if isEmptyString(emailOrID) {
+		return nil, errors.New("emailOrID cannot be nil for Get operation")
+	}
+
+	endpoint := fmt.Sprintf("/developers/%v", *emailOrID)
+	req, err := s.client.NewRequest("GET", endpoint, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var developer Developer
+	_, err = s.client.Do(ctx, req, &developer)
+	if err != nil {
+		return nil, err
+	}
+	return &developer, nil
+}
+
+// GetByCustomID fetches a Developer in Kong.
+func (s *DeveloperService) GetByCustomID(ctx context.Context,
+	customID *string) (*Developer, error) {
+
+	if isEmptyString(customID) {
+		return nil, errors.New("customID cannot be nil for Get operation")
+	}
+
+	type QS struct {
+		CustomID string `url:"custom_id,omitempty"`
+	}
+
+	req, err := s.client.NewRequest("GET", "/developers",
+		&QS{CustomID: *customID}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	type Response struct {
+		Data []Developer
+	}
+	var resp Response
+	_, err = s.client.Do(ctx, req, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(resp.Data) == 0 {
+		return nil, NewAPIError(http.StatusNotFound, "Not found")
+	}
+
+	return &resp.Data[0], nil
+}
+
+// Update updates a Developer in Kong
+func (s *DeveloperService) Update(ctx context.Context,
+	developer *Developer) (*Developer, error) {
+
+	if isEmptyString(developer.ID) {
+		return nil, errors.New("ID cannot be nil for Update operation")
+	}
+
+	endpoint := fmt.Sprintf("/developers/%v", *developer.ID)
+	req, err := s.client.NewRequest("PATCH", endpoint, nil, developer)
+	if err != nil {
+		return nil, err
+	}
+
+	var updatedAPI Developer
+	_, err = s.client.Do(ctx, req, &updatedAPI)
+	if err != nil {
+		return nil, err
+	}
+	return &updatedAPI, nil
+}
+
+// Delete deletes a Developer in Kong
+func (s *DeveloperService) Delete(ctx context.Context,
+	emailOrID *string) error {
+
+	if isEmptyString(emailOrID) {
+		return errors.New("emailOrID cannot be nil for Delete operation")
+	}
+
+	endpoint := fmt.Sprintf("/developers/%v", *emailOrID)
+	req, err := s.client.NewRequest("DELETE", endpoint, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = s.client.Do(ctx, req, nil)
+	return err
+}
+
+// List fetches a list of Developers in Kong.
+// opt can be used to control pagination.
+func (s *DeveloperService) List(ctx context.Context,
+	opt *ListOpt) ([]*Developer, *ListOpt, error) {
+	data, next, err := s.client.list(ctx, "/developers", opt)
+	if err != nil {
+		return nil, nil, err
+	}
+	var developers []*Developer
+
+	for _, object := range data {
+		b, err := object.MarshalJSON()
+		if err != nil {
+			return nil, nil, err
+		}
+		var developer Developer
+		err = json.Unmarshal(b, &developer)
+		if err != nil {
+			return nil, nil, err
+		}
+		developers = append(developers, &developer)
+	}
+
+	return developers, next, nil
+}
+
+// ListAll fetches all Developers in Kong.
+// This method can take a while if there
+// a lot of Developers present.
+func (s *DeveloperService) ListAll(ctx context.Context) ([]*Developer, error) {
+	var developers, data []*Developer
+	var err error
+	opt := &ListOpt{Size: pageSize}
+
+	for opt != nil {
+		data, opt, err = s.List(ctx, opt)
+		if err != nil {
+			return nil, err
+		}
+		developers = append(developers, data...)
+	}
+	return developers, nil
+}
---
+++ kong/developer_service_test.go
@@ -0,0 +1,145 @@
+package kong
+
+import (
+	"testing"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDevelopersService(T *testing.T) {
+	assert := assert.New(T)
+
+	client, err := NewTestClient(nil, nil)
+	assert.Nil(err)
+	assert.NotNil(client)
+
+	developer := &Developer{
+		Email:    String("foo@example.com"),
+		CustomID: String("custom_id_foo"),
+	}
+
+	createdDeveloper, err := client.Developers.Create(defaultCtx, developer)
+	assert.Nil(err)
+	assert.NotNil(createdDeveloper)
+
+	developer, err = client.Developers.Get(defaultCtx, createdDeveloper.ID)
+	assert.Nil(err)
+	assert.NotNil(developer)
+
+	developer, err = client.Developers.GetByCustomID(defaultCtx,
+		String("does-not-exist"))
+	assert.NotNil(err)
+	assert.Nil(developer)
+
+	developer, err = client.Developers.GetByCustomID(defaultCtx,
+		String("custom_id_foo"))
+	assert.Nil(err)
+	assert.NotNil(developer)
+
+	developer.Email = String("bar@example.com")
+	developer, err = client.Developers.Update(defaultCtx, developer)
+	assert.Nil(err)
+	assert.NotNil(developer)
+	assert.Equal("bar@example.com", *developer.Email)
+
+	err = client.Developers.Delete(defaultCtx, createdDeveloper.ID)
+	assert.Nil(err)
+
+	// ID can be specified
+	id := uuid.NewV4().String()
+	developer = &Developer{
+		Email: String("foo@example.com"),
+		ID:    String(id),
+	}
+
+	createdDeveloper, err = client.Developers.Create(defaultCtx, developer)
+	assert.Nil(err)
+	assert.NotNil(createdDeveloper)
+	assert.Equal(id, *createdDeveloper.ID)
+
+	err = client.Developers.Delete(defaultCtx, createdDeveloper.ID)
+	assert.Nil(err)
+}
+
+func TestDeveloperListEndpoint(T *testing.T) {
+	assert := assert.New(T)
+
+	client, err := NewTestClient(nil, nil)
+	assert.Nil(err)
+	assert.NotNil(client)
+
+	// fixtures
+	developers := []*Developer{
+		{
+			Email: String("foo1@example.com"),
+		},
+		{
+			Email: String("foo2@example.com"),
+		},
+		{
+			Email: String("foo3@example.com"),
+		},
+	}
+
+	// create fixturs
+	for i := 0; i < len(developers); i++ {
+		developer, err := client.Developers.Create(defaultCtx, developers[i])
+		assert.Nil(err)
+		assert.NotNil(developer)
+		developers[i] = developer
+	}
+
+	developersFromKong, next, err := client.Developers.List(defaultCtx, nil)
+	assert.Nil(err)
+	assert.Nil(next)
+	assert.NotNil(developersFromKong)
+	assert.Equal(3, len(developersFromKong))
+
+	// check if we see all developers
+	assert.True(compareDevelopers(developers, developersFromKong))
+
+	// Test pagination
+	developersFromKong = []*Developer{}
+
+	// first page
+	page1, next, err := client.Developers.List(defaultCtx, &ListOpt{Size: 1})
+	assert.Nil(err)
+	assert.NotNil(next)
+	assert.NotNil(page1)
+	assert.Equal(1, len(page1))
+	developersFromKong = append(developersFromKong, page1...)
+
+	// last page
+	next.Size = 2
+	page2, next, err := client.Developers.List(defaultCtx, next)
+	assert.Nil(err)
+	assert.Nil(next)
+	assert.NotNil(page2)
+	assert.Equal(2, len(page2))
+	developersFromKong = append(developersFromKong, page2...)
+
+	assert.True(compareDevelopers(developers, developersFromKong))
+
+	developers, err = client.Developers.ListAll(defaultCtx)
+	assert.Nil(err)
+	assert.NotNil(developers)
+	assert.Equal(3, len(developers))
+
+	for i := 0; i < len(developers); i++ {
+		assert.Nil(client.Developers.Delete(defaultCtx, developers[i].ID))
+	}
+}
+
+func compareDevelopers(expected, actual []*Developer) bool {
+	var expectedEmails, actualEmails []string
+	for _, developer := range expected {
+		expectedEmails = append(expectedEmails, *developer.Email)
+	}
+
+	for _, developer := range actual {
+		actualEmails = append(actualEmails, *developer.Email)
+	}
+
+	return (compareSlices(expectedEmails, actualEmails))
+}
