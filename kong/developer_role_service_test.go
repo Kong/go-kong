93d4c6d05d709ca7d8a2da26dfd0be8d9b86b533
MOREL Matthieu <matthieu.morel@cnp.fr>
2021-03-25 09:46:39 +0100

    feat: support paginated dev and RBAC roles
    Add pagination options to the List functions and add a ListAll function
    for the developer role and RBAC role services.

--- kong/developer_role_service_test.go
+++ kong/developer_role_service_test.go
@@ -71,14 +71,97 @@ func TestDeveloperRoleServiceList(T *testing.T) {
 	createdRoleB, err := client.DeveloperRoles.Create(defaultCtx, roleB)
 	assert.Nil(err)
 
-	roles, err := client.DeveloperRoles.List(defaultCtx)
+	roles, next, err := client.DeveloperRoles.List(defaultCtx, nil)
 	assert.Nil(err)
+	assert.Nil(next)
 	assert.NotNil(roles)
-	// Counts default roles (super-admin, admin, read-only)
-	assert.Equal(5, len(roles))
+	assert.Equal(2, len(roles))
 
 	err = client.DeveloperRoles.Delete(defaultCtx, createdRoleA.ID)
 	assert.Nil(err)
 	err = client.DeveloperRoles.Delete(defaultCtx, createdRoleB.ID)
 	assert.Nil(err)
+}
+
+func TestDeveloperRoleListEndpoint(T *testing.T) {
+	runWhenEnterprise(T, ">=0.33.0", true)
+	assert := assert.New(T)
+
+	client, err := NewTestClient(nil, nil)
+	assert.Nil(err)
+	assert.NotNil(client)
+
+	// fixtures
+	roles := []*DeveloperRole{
+		{
+			Name: String("roleA"),
+		},
+		{
+			Name: String("roleB"),
+		},
+		{
+			Name: String("roleC"),
+		},
+	}
+
+	// create fixturs
+	for i := 0; i < len(roles); i++ {
+		role, err := client.DeveloperRoles.Create(defaultCtx, roles[i])
+		assert.Nil(err)
+		assert.NotNil(role)
+		roles[i] = role
+	}
+
+	rolesFromKong, next, err := client.DeveloperRoles.List(defaultCtx, nil)
+	assert.Nil(err)
+	assert.Nil(next)
+	assert.NotNil(rolesFromKong)
+	assert.Equal(3, len(rolesFromKong))
+
+	// check if we see all developerRoles
+	assert.True(compareDeveloperRoles(roles, rolesFromKong))
+
+	// Test pagination
+	rolesFromKong = []*DeveloperRole{}
+
+	// first page
+	page1, next, err := client.DeveloperRoles.List(defaultCtx, &ListOpt{Size: 1})
+	assert.Nil(err)
+	assert.NotNil(next)
+	assert.NotNil(page1)
+	assert.Equal(1, len(page1))
+	rolesFromKong = append(rolesFromKong, page1...)
+
+	// last page
+	next.Size = 2
+	page2, next, err := client.DeveloperRoles.List(defaultCtx, next)
+	assert.Nil(err)
+	assert.Nil(next)
+	assert.NotNil(page2)
+	assert.Equal(2, len(page2))
+	rolesFromKong = append(rolesFromKong, page2...)
+
+	assert.True(compareDeveloperRoles(roles, rolesFromKong))
+
+	roles, err = client.DeveloperRoles.ListAll(defaultCtx)
+	assert.Nil(err)
+	assert.NotNil(roles)
+	assert.Equal(3, len(roles))
+
+	for i := 0; i < len(roles); i++ {
+		assert.Nil(client.DeveloperRoles.Delete(defaultCtx, roles[i].ID))
+	}
+}
+
+func compareDeveloperRoles(expected, actual []*DeveloperRole) bool {
+	var expectedNames, actualNames []string
+	for _, role := range expected {
+		expectedNames = append(expectedNames, *role.Name)
+	}
+
+	for _, role := range actual {
+		actualNames = append(actualNames, *role.Name)
+	}
+
+	return (compareSlices(expectedNames, actualNames))
 }
