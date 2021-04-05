bf757ce53b7210b089c3af180a77ad3d7e1265d3
MOREL Matthieu <matthieu.morel@cnp.fr>
2021-04-05 11:45:24 +0200

    list for upstream node health

--- kong/upstream_node_health_service.go
+++ kong/upstream_node_health_service.go
@@ -3,6 +3,7 @@ package kong
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 )
 
@@ -19,12 +20,30 @@ type UpstreamNodeHealthService service
 
 // List fetches a list of Upstream Node Healths in Kong.
 // opt can be used to control pagination.
-func (s *UpstreamNodeHealthService) List(
-	ctx context.Context,
-	upstreamNameOrID *string,
-	opt *ListOpt,
-) ([]*UpstreamNodeHealth, *ListOpt, error) {
+func (s *UpstreamNodeHealthService) List(ctx context.Context,
+	upstreamNameOrID *string, opt *ListOpt) ([]*UpstreamNodeHealth, *ListOpt, error) {
+	if isEmptyString(upstreamNameOrID) {
+		return nil, nil, errors.New("upstreamNameOrID cannot be nil for Get operation")
+	}
 	endpoint := fmt.Sprintf("/upstreams/%v/health", *upstreamNameOrID)
+
+	return s.ListByEndpointAndOpt(ctx, endpoint, opt)
+}
+
+// ListAll fetches all Upstream Node Healths in Kong.
+// This method can take a while if there are
+// a lot of Upstream Node Healths present.
+func (s *UpstreamNodeHealthService) ListAll(ctx context.Context,
+	upstreamNameOrID *string) ([]*UpstreamNodeHealth, error) {
+	if isEmptyString(upstreamNameOrID) {
+		return nil, errors.New("upstreamNameOrID cannot be nil for Get operation")
+	}
+	endpoint := fmt.Sprintf("/upstreams/%v/health", *upstreamNameOrID)
+	return s.ListAllByEndpointAndOpt(ctx, endpoint, newOpt(nil))
+}
+
+func (s *UpstreamNodeHealthService) ListByEndpointAndOpt(ctx context.Context,
+	endpoint string, opt *ListOpt) ([]*UpstreamNodeHealth, *ListOpt, error) {
 	data, next, err := s.client.list(ctx, endpoint, opt)
 	if err != nil {
 		return nil, nil, err
@@ -43,23 +62,22 @@ func (s *UpstreamNodeHealthService) List(
 	return upstreamNodeHealths, next, nil
 }
 
-// ListAll fetches all Upstream Node Healths in Kong.
-// This method can take a while if there are
-// a lot of Upstream Node Healths present.
-func (s *UpstreamNodeHealthService) ListAll(
-	ctx context.Context,
-	upstreamNameOrID *string,
-) ([]*UpstreamNodeHealth, error) {
-	var upstreamNodeHealths, data []*UpstreamNodeHealth
-	var err error
-	opt := &ListOpt{Size: pageSize}
+func (s *UpstreamNodeHealthService) ListAllByEndpointAndOpt(ctx context.Context,
+	endpoint string, opt *ListOpt) ([]*UpstreamNodeHealth, error) {
+	data, err := s.client.listAll(ctx, endpoint, opt, false)
+	if err != nil {
+		return nil, err
+	}
+	var upstreamNodeHealths []*UpstreamNodeHealth
 
-	for opt != nil {
-		data, opt, err = s.List(ctx, upstreamNameOrID, opt)
+	for _, object := range data {
+		var upstreamNodeHealth UpstreamNodeHealth
+		err = json.Unmarshal(object, &upstreamNodeHealth)
 		if err != nil {
 			return nil, err
 		}
-		upstreamNodeHealths = append(upstreamNodeHealths, data...)
+		upstreamNodeHealths = append(upstreamNodeHealths, &upstreamNodeHealth)
 	}
+
 	return upstreamNodeHealths, nil
 }
