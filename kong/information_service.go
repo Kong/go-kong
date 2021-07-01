26f0e0eec8e8a6e20eb149e051c331e022cc286c
Matthieu MOREL <mmorel-35@users.noreply.github.com>
2021-07-01 16:40:41 +0200

    Defines the Information Service on top of Kong's Root endpoint (#65)
    * Defines the Information Service on top of Kong's Root endpoint
    
    * typo in comment
    
    * shouldn't be empty
    
    * Update documentation
    
    * no s for information
    
    * One endpoint to rule them all
    
    * gofmt
    
    * remove context parameter
    
    * integration of the feedbacks
    
    * Update information_service.go
    
    * update deepcopy
    
    * lowercase convert
    
    * Update kong/information_service.go
    
    Co-authored-by: Michał Flendrich <m.flendrich@gmail.com>
    
    * Update kong/information_service_test.go
    
    Co-authored-by: Michał Flendrich <m.flendrich@gmail.com>
    
    * Update kong/information_service.go
    
    Co-authored-by: Michał Flendrich <m.flendrich@gmail.com>
    
    * Integration test for Info
    
    * Cover IsInMemory and isRBACEnabled
    
    * Remove deepCopy
    
    * chore(*): docstring
    
    Co-authored-by: Michał Flendrich <m.flendrich@gmail.com>

---
+++ kong/information_service.go
@@ -0,0 +1,47 @@
+package kong
+
+import (
+	"context"
+	"encoding/json"
+)
+
+// AbstractInfoService handles Kong's Information.
+type AbstractInfoService interface {
+	// Get retrieves the general runtime information about the Kong gateway.
+	Get(ctx context.Context) (*Info, error)
+}
+
+type InfoService service
+
+// IsInMemory check if Kong is in memory
+func (r *RuntimeConfiguration) IsInMemory() bool {
+	return "off" == r.Database
+}
+
+// IsRBACEnabled check if RBAC are enabled
+func (r *RuntimeConfiguration) IsRBACEnabled() bool {
+	return "on" == r.RBAC
+}
+
+// convert convert an object to another through json marshalling
+// unmarshalling
+func convert(from, to interface{}) error {
+	bytes, err := json.Marshal(from)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(bytes, to)
+}
+
+// Get retrieves the high-level metadata of a Kong instance.
+func (s *InfoService) Get(ctx context.Context) (*Info, error) {
+	information, err := s.client.Root(ctx)
+	if err != nil {
+		return nil, err
+	}
+	var info Info
+	if err := convert(information, &info); err != nil {
+		return nil, err
+	}
+	return &info, nil
+}
--- kong/types.go
+++ kong/types.go
@@ -438,3 +438,16 @@ type DeveloperRole struct {
 	ID        *string `json:"id,omitempty" yaml:"id,omitempty"`
 	Name      *string `json:"name,omitempty" yaml:"name,omitempty"`
 }
+
+// Info represents the information concerning Kong.
+type Info struct {
+	Version       string                `json:"version,omitempty" yaml:"version,omitempty"`
+	Configuration *RuntimeConfiguration `json:"configuration,omitempty" yaml:"configuration,omitempty"`
+}
+
+// RuntimeConfiguration represents the runtime configuration of Kong.
+type RuntimeConfiguration struct {
+	Database string `json:"database,omitempty" yaml:"database,omitempty"`
+	Portal   bool   `json:"portal,omitempty" yaml:"portal,omitempty"`
+	RBAC     string `json:"rbac,omitempty" yaml:"rbac,omitempty"`
+}
