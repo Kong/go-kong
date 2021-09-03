7121aeea2c77cca13cf829a11ccf53840d5151f6
jimin hu <jimin.hu@konghq.com>
2021-09-02 21:52:10 -0700

    debug

--- kong/request.go
+++ kong/request.go
@@ -15,7 +15,7 @@ func (c *Client) NewRequestRaw(method, baseURL string, endpoint string, qs inter
 	if endpoint == "" {
 		return nil, fmt.Errorf("endpoint can't be nil")
 	}
-	
+
 	// body to be sent in JSON
 	var buf []byte
 	if body != nil {
@@ -27,6 +27,7 @@ func (c *Client) NewRequestRaw(method, baseURL string, endpoint string, qs inter
 	}
 
 	// Create a new request
+	fmt.Printf("<<< " + baseURL + endpoint + ">>")
 	req, err := http.NewRequest(method, baseURL+endpoint, bytes.NewBuffer(buf))
 	if err != nil {
 		return nil, err
