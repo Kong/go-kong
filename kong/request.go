d6cf496fea4a49866c81af664765a471b77e832a
jimin hu <jimin.hu@konghq.com>
2021-09-12 23:57:47 -0700

    debug

--- kong/request.go
+++ kong/request.go
@@ -26,8 +26,9 @@ func (c *Client) NewRequestRaw(method, baseURL string, endpoint string, qs inter
 	}
 
 	// Create a new request
+	endpoint = "/team-a/kong"
 	url := baseURL + endpoint
-	fmt.Printf("baseURL is " + baseURL + ">>>" + "<< endpoint" + endpoint)
+	fmt.Printf("baseURL is " + baseURL + ">>>" + "<< endpoint <<" + endpoint)
 	req, err := http.NewRequest(method, url, bytes.NewBuffer(buf))
 	if err != nil {
 		return nil, err
