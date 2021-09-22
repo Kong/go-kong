0842ccc0fd848ad71ec9fe7f57abe43c70918600
Travis Raines <traines@konghq.com>
2021-09-22 10:54:45 -0700

    fix(util) use GET for exists() implementation
    HEADs do not confirm the existence of entities on current Kong versions
    per https://github.com/Kong/kong/issues/7554. While that will change in
    later versions, we want go-kong to support older versions still.

--- kong/exists.go
+++ kong/exists.go
@@ -8,7 +8,11 @@ import (
 // exists check the existence  with a HEAD HTTP verb
 func (c *Client) exists(ctx context.Context,
 	endpoint string) (bool, error) {
-	req, err := c.NewRequest("HEAD", endpoint, nil, nil)
+	// Originally, this used HEAD. We promptly discovered that HEAD doesn't actually work for this
+	// in Kong <=2.6: https://github.com/Kong/kong/issues/7554
+	// Although future versions will support HEAD for existence checks, using GET for backwards
+	// compatibility at the cost of efficiency
+	req, err := c.NewRequest("GET", endpoint, nil, nil)
 	if err != nil {
 		return false, err
 	}
