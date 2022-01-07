43c45e0586065af08fa681a4b31c8f2b2cec5e62
Travis Raines <571832+rainest@users.noreply.github.com>
2022-01-07 16:44:55 +0200

    feat(info) add config ready function

--- kong/information_service.go
+++ kong/information_service.go
@@ -3,12 +3,17 @@ package kong
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 )
 
 // AbstractInfoService handles Kong's Information.
 type AbstractInfoService interface {
 	// Get retrieves the general runtime information about the Kong gateway.
 	Get(ctx context.Context) (*Info, error)
+	// IsConfigReady returns true if the Kong instance has configuration and false if it does not
+	IsConfigReady(ctx context.Context) (bool, error)
 }
 
 type InfoService service
@@ -44,4 +49,25 @@ func (s *InfoService) Get(ctx context.Context) (*Info, error) {
 		return nil, err
 	}
 	return &info, nil
+}
+
+// IsConfigReady returns true if the Kong instance has configuration and false if it does not
+func (s *InfoService) IsConfigReady(ctx context.Context) (bool, error) {
+	req, err := s.client.NewRequest("GET", "/config/ready/", nil, nil)
+	if err != nil {
+		return false, err
+	}
+
+	resp, err := s.client.Do(ctx, req, nil)
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusServiceUnavailable {
+		if err != nil {
+			return false, fmt.Errorf("could not determine config state: %v %v", resp.StatusCode, err)
+		}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			body = []byte(err.Error())
+		}
+		return false, fmt.Errorf("could not determine config state: %v %v", resp.StatusCode, body)
+	}
+	return resp.StatusCode == http.StatusOK, nil
 }
--- kong/information_service_test.go
+++ kong/information_service_test.go
@@ -1,8 +1,12 @@
 package kong
 
 import (
+	"context"
+	"net/http"
 	"reflect"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -46,4 +50,67 @@ func TestConvert(T *testing.T) {
 	assert.True(reflect.DeepEqual(expected, &actual))
 	assert.False(actual.Configuration.IsInMemory())
 	assert.True(actual.Configuration.IsRBACEnabled())
+}
+
+func TestIsConfigReady(T *testing.T) {
+	for _, tt := range []struct {
+		name      string
+		status    int
+		wantReady bool
+		wantErr   bool
+	}{
+		{
+			name:      "ready",
+			status:    http.StatusOK,
+			wantReady: true,
+			wantErr:   false,
+		},
+		{
+			name:      "not ready",
+			status:    http.StatusServiceUnavailable,
+			wantReady: false,
+			wantErr:   false,
+		},
+		{
+			name:      "unexpected status",
+			status:    http.StatusNotFound,
+			wantReady: false,
+			wantErr:   true,
+		},
+	} {
+		T.Run(tt.name, func(T *testing.T) {
+			assert := assert.New(T)
+			wait := &sync.WaitGroup{}
+			server := mockConfigReady(T, tt.status, wait)
+			mockURL := "http://localhost:32997"
+			client, err := NewTestClient(&mockURL, nil)
+			assert.NoError(err)
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+			defer cancel()
+			ready, err := client.Info.IsConfigReady(ctx)
+			assert.Equal(tt.wantReady, ready)
+			if (err == nil) == tt.wantErr {
+				T.Errorf("processCredential() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			assert.NoError(server.Shutdown(ctx))
+			wait.Wait()
+		})
+	}
+}
+
+func mockConfigReady(T *testing.T, status int, wait *sync.WaitGroup) *http.Server {
+	assert := assert.New(T)
+	mux := http.NewServeMux()
+	mux.Handle("/config/ready/", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(status)
+	}))
+	server := &http.Server{Addr: ":32997", Handler: mux}
+	go func() {
+		defer wait.Done()
+		wait.Add(1)
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			assert.NoError(err)
+		}
+	}()
+	return server
 }
