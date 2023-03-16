3d528e3564a4bc4fa08b2bb5eb92ddf543a07cab
Patryk Małek <patryk.malek@konghq.com>
2023-03-16 12:56:10 +0100

    ci: fix setup kong scripts

--- .ci/setup_kong.sh
+++ .ci/setup_kong.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+source $(dirname "$0")/_common.sh
+
 KONG_IMAGE=${KONG_IMAGE_REPO:-kong}:${KONG_IMAGE_TAG:-3.1.1}
 NETWORK_NAME=kong-test
 
@@ -12,38 +14,6 @@ KONG_DB_PASSWORD=kong
 KONG_PG_HOST=pg
 
 GATEWAY_CONTAINER_NAME=kong
-
-waitContainer() {
-  for try in {1..100}; do
-    echo "waiting for $1.."
-    nc -w 1 localhost $2 && break;
-    sleep $3
-  done
-}
-
-function deploy_pg()
-{
-  # Start a PostgreSQL container
-  docker run --rm -d --name $PG_CONTAINER_NAME \
-    --network=$NETWORK_NAME \
-    -p 5432:5432 \
-    -e "POSTGRES_USER=$DATABASE_USER" \
-    -e "POSTGRES_DB=$DATABASE_NAME" \
-    -e "POSTGRES_PASSWORD=$KONG_DB_PASSWORD" \
-    postgres:9.6
-
-  waitContainer "PostgreSQL" 5432 0.2
-
-  for try in {1..10}; do
-    # Prepare the Kong database
-    docker run --rm --network=$NETWORK_NAME \
-      -e "KONG_DATABASE=postgres" \
-      -e "KONG_PG_HOST=$KONG_PG_HOST" \
-      -e "KONG_PG_PASSWORD=$KONG_DB_PASSWORD" \
-      -e "KONG_PASSWORD=$KONG_DB_PASSWORD" \
-      $KONG_IMAGE kong migrations bootstrap && break
-  done
-}
 
 function deploy_kong_postgres()
 {
@@ -91,11 +61,6 @@ function deploy_kong_dbless()
   waitContainer "Kong" 8001 0.2
 }
 
-function create_network()
-{
-  docker network inspect $NETWORK_NAME 2>/dev/null >/dev/null || docker network create $NETWORK_NAME
-}
-
 while [[ $# -gt 0 ]]; do
   case $1 in
     --dbless)
@@ -120,6 +85,7 @@ if [[ "${DBMODE}" == "off" ]]; then
 elif [[ "${DBMODE}" == "postgres" ]]; then
   create_network
   deploy_pg
+  perform_migrations
   deploy_kong_postgres
 else
   echo "ERROR: no dbmode specified. Run this script with --dbless or --postgres"
