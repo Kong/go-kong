072f0c3513ec11426903bf86089001b73aa519cb
Yi Tao <tao.yi@konghq.com>
2024-01-12 14:31:34 +0800

    extract details from response

--- kong/response.go
+++ kong/response.go
@@ -68,7 +68,10 @@ func hasError(res *http.Response) error {
 }
 
 func extractErrDetails(res *http.Response, body []byte) (any, bool) {
-	// firstly deal with certain status code.
+	// Firstly extract `details` field from response.
+	if detailsFromRespBody := detailsFromBodyDetailsField(body); detailsFromRespBody != nil {
+		return detailsFromRespBody, true
+	}
 	switch res.StatusCode {
 	case http.StatusTooManyRequests:
 		return extractErrTooManyRequestsDetails(res)
