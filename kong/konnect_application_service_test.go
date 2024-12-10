e2e7a1e67a904c9b87b964399b9ef0ffbc40c91b
Jay Shah <jay.shah@konghq.com>
2024-12-10 18:02:31 -0500

    feat: konnect application auth client apis

---
+++ kong/konnect_application_service_test.go
@@ -0,0 +1,102 @@
+package kong
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestKonnectApplicationService_Create(T *testing.T) {
+	RunWhenDBMode(T, "postgres")
+	RunWhenKong(T, ">=3.1.0")
+
+	assert := assert.New(T)
+	require := require.New(T)
+
+	client, err := NewTestClient(nil, nil)
+	assert.NoError(err)
+	assert.NotNil(client)
+
+	var (
+		clientID        = uuid.NewString()
+		consumerGroup   = []string{uuid.NewString()}
+		scopes          = []string{"/auth"}
+		authStrategyID  = uuid.NewString()
+		exhaustedScopes = []string{"/eauth"}
+		orgID           = uuid.NewString()
+		developerID     = uuid.NewString()
+		createdAt       = time.Now().Unix()
+	)
+
+	kaa := &KonnectApplication{
+		ID:              &clientID,
+		ClientID:        clientID,
+		ConsumerGroups:  consumerGroup,
+		Scopes:          scopes,
+		AuthStrategyID:  &authStrategyID,
+		ExhaustedScopes: exhaustedScopes,
+		ApplicationContext: &ApplicationContext{
+			OrganizationID: &orgID,
+			DeveloperID:    &developerID,
+		},
+		CreatedAt: createdAt,
+	}
+	createResponse, err := client.KonnectApplication.Create(defaultCtx, kaa)
+	require.NoError(err)
+	require.NotNil(createResponse)
+	require.Equal(createResponse.ClientID, clientID)
+	require.Equal(createResponse.CreatedAt, createdAt)
+	require.Equal(createResponse.ConsumerGroups, consumerGroup)
+	require.Equal(createResponse.Scopes, scopes)
+	require.Equal(createResponse.ExhaustedScopes, exhaustedScopes)
+}
+
+func TestKonnectApplicationService_ListAll(T *testing.T) {
+	RunWhenDBMode(T, "postgres")
+	RunWhenKong(T, ">=3.1.0")
+
+	require := require.New(T)
+
+	client, err := NewTestClient(nil, nil)
+	require.NoError(err)
+	require.NotNil(client)
+
+	var (
+		expectedKonnectApplications = 10
+		consumerGroup               = []string{uuid.NewString()}
+		scopes                      = []string{"/auth"}
+		authStrategyID              = uuid.NewString()
+		exhaustedScopes             = []string{"/eauth"}
+		orgID                       = uuid.NewString()
+		developerID                 = uuid.NewString()
+		createdAt                   = time.Now().Unix()
+	)
+
+	kaa := &KonnectApplication{
+		ConsumerGroups:  consumerGroup,
+		Scopes:          scopes,
+		AuthStrategyID:  &authStrategyID,
+		ExhaustedScopes: exhaustedScopes,
+		ApplicationContext: &ApplicationContext{
+			OrganizationID: &orgID,
+			DeveloperID:    &developerID,
+		},
+		CreatedAt: createdAt,
+	}
+
+	for i := 0; i < expectedKonnectApplications; i++ {
+		clientID := uuid.NewString()
+		kaa.ID = &clientID
+		kaa.ClientID = clientID
+		createResponse, err := client.KonnectApplication.Create(defaultCtx, kaa)
+		require.NoError(err)
+		require.NotNil(createResponse)
+	}
+
+	listKonnectApplicationResponse, err := client.KonnectApplication.ListAll(defaultCtx)
+	require.NoError(err)
+	require.Len(listKonnectApplicationResponse, expectedKonnectApplications)
+}
