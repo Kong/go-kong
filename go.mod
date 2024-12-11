9ee4ac9a0987391ed9f8e1f9a16609b34eb5eb0b
Jay Shah <jay.shah@konghq.com>
2024-12-11 15:23:12 -0500

    add test for list

--- go.mod
+++ go.mod
@@ -40,6 +40,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/samber/lo v1.47.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
--- kong/konnect_application_service_test.go
+++ kong/konnect_application_service_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -110,4 +111,93 @@ func TestKonnectApplicationService_ListAll(T *testing.T) {
 	listKonnectApplicationResponse, err := client.KonnectApplication.ListAll(defaultCtx)
 	require.NoError(err)
 	require.Len(listKonnectApplicationResponse, expectedKonnectApplications)
+}
+
+func TestKonnectApplicationService_List(T *testing.T) {
+	RunWhenDBMode(T, "postgres")
+	RunWhenEnterprise(T, ">=3.6.0", RequiredFeatures{})
+
+	assert := assert.New(T)
+	require := require.New(T)
+
+	client, err := NewTestClient(nil, nil)
+	require.NoError(err)
+	require.NotNil(client)
+
+	var (
+		expectedKonnectApplications = 10
+		consumerGroup               = []string{uuid.NewString()}
+		scopes                      = []string{"/auth"}
+		authStrategyID              = uuid.NewString()
+		exhaustedScopes             = []string{"/eauth"}
+		orgID                       = uuid.NewString()
+		developerID                 = uuid.NewString()
+		createdAt                   = time.Now().Unix()
+		tagA                        = "list1"
+		tagB                        = "list2"
+	)
+
+	kaa := &KonnectApplication{
+		ConsumerGroups:  consumerGroup,
+		Scopes:          scopes,
+		AuthStrategyID:  &authStrategyID,
+		ExhaustedScopes: exhaustedScopes,
+		ApplicationContext: &ApplicationContext{
+			OrganizationID: &orgID,
+			DeveloperID:    &developerID,
+		},
+		CreatedAt: createdAt,
+	}
+
+	for i := 0; i < expectedKonnectApplications/2; i++ {
+		clientID := uuid.NewString()
+		kaa.ID = &clientID
+		kaa.ClientID = clientID
+		kaa.Tags = lo.ToPtr([]string{tagA})
+		createResponse, err := client.KonnectApplication.Create(defaultCtx, kaa)
+		require.NoError(err)
+		require.NotNil(createResponse)
+
+		T.Cleanup(func() {
+			assert.NoError(client.KonnectApplication.Delete(context.Background(), createResponse.ID))
+		})
+	}
+
+	for i := 0; i < expectedKonnectApplications/2; i++ {
+		clientID := uuid.NewString()
+		kaa.ID = &clientID
+		kaa.ClientID = clientID
+		kaa.Tags = lo.ToPtr([]string{tagB})
+		createResponse, err := client.KonnectApplication.Create(defaultCtx, kaa)
+		require.NoError(err)
+		require.NotNil(createResponse)
+
+		T.Cleanup(func() {
+			assert.NoError(client.KonnectApplication.Delete(context.Background(), createResponse.ID))
+		})
+	}
+
+	// Filter by tag listA
+	listKonnectApplicationResponseByTagA, _, err := client.KonnectApplication.List(defaultCtx, &ListOpt{
+		Size: 10,
+		Tags: []*string{lo.ToPtr(tagA)},
+	})
+	require.NoError(err)
+	require.Len(listKonnectApplicationResponseByTagA, 5)
+
+	// Filter by tag listB
+	listKonnectApplicationResponseByTagB, _, err := client.KonnectApplication.List(defaultCtx, &ListOpt{
+		Size: 10,
+		Tags: []*string{lo.ToPtr(tagB)},
+	})
+	require.NoError(err)
+	require.Len(listKonnectApplicationResponseByTagB, 5)
+
+	size := 2
+	// Filter by size
+	listKonnectApplicationResponseBySize, _, err := client.KonnectApplication.List(defaultCtx, &ListOpt{
+		Size: size,
+	})
+	require.NoError(err)
+	require.Len(listKonnectApplicationResponseBySize, size)
 }
